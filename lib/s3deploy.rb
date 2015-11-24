0371a7d6e399305c1b02aed6eec90bb290a887de
Ryan Mark <ryan@mrk.cc>
2015-11-24 15:54:45 -0500

    gz topojson, geojson, kml, txt

--- lib/s3deploy.rb
+++ lib/s3deploy.rb
@@ -4,16 +4,15 @@ require 's3deploy/deployer'
 # Top level module for s3deploy, also a singleton instance of a Deployer
 module S3deploy
   DEFAULT_GZIP = [
-    /\.js$/, /\.css$/, /\.html?$/, /\.svg$/, /\.md$/,
-    /\.json$/, /\.csv$/, /\.tsv$/,
+    /\.js$/, /\.css$/, /\.html?$/, /\.svg$/, /\.md$/, /\.txt$/,
+    /\.json$/, /\.topojson$/, /\.geojson$/, /\.kml$/,
+    /\.csv$/, /\.tsv$/,
     /\.eot$/, /\.ttf$/, /\.woff$/
   ]
   class << self
-    attr_reader :config
     def configure(&block)
-      @config = Config.new
-      @config.instance_eval(&block)
-      @config.apply_environment_settings!
+      config.instance_eval(&block)
+      config.apply_environment_settings!
 
       @deployer = Deployer.new(
         access_key_id: config.access_key_id,
@@ -26,10 +25,18 @@ module S3deploy
       )
     end
 
+    def config
+      @config ||= Config.new
+    end
+
     def deploy!
       config.before_deploy if config.before_deploy
       @deployer.deploy!
       config.after_deploy if config.after_deploy
+    end
+
+    def destroy!
+      @deployer.delete!
     end
 
     def new(opts)
